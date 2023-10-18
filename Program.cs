1a63804dcf29a8866fc7a5a284c6c67c7a9b4cc5
Robokat9 <kolesovakaterinapo433@gmail.com>
2023-10-19 01:57:42 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telegram_bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
